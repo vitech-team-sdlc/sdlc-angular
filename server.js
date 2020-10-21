82b5ac7b51a14c3016c70b0561bd9ea9fd3d789b
serhiy-krupka-vitech <serhiy.krupka@vitech.com.ua>
2020-10-21 17:39:24 +0300

    chore(ci): change on express as exec engine

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+// Create link to Angular build directory
+const distDir = __dirname + "/dist/mood-feed-frontend";
+app.use(express.static(distDir));
+
+const server = app.listen(process.env.PORT || 8080, function () {
+  const port = server.address().port;
+  console.log("App now running on port", port, distDir);
+});
